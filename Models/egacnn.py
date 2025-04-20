13420b10a4263d3d7e29e8cacbc878fce9ab643f
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-04-20 18:29:12 +0700

    Feat: Validation and accuracy graph

--- Models/egacnn.py
+++ Models/egacnn.py
@@ -31,20 +31,24 @@ class square_padding:
 
 # Transform the images to be used
 transform_format = transforms.Compose([square_padding(), transforms.Resize((144,144)), transforms.Grayscale(1), transforms.ToTensor(), transforms.Normalize(0.5,0.5)])
-# transform_augment = transforms.RandomApply([transforms.RandomRotation(15), transforms.RandomVerticalFlip(), transforms.RandomHorizontalFlip()])
-# transform_augment_format = transforms.Compose([transform_augment, transform_format])
+transform_augment = transforms.RandomChoice([transforms.RandomRotation((5)), transforms.RandomHorizontalFlip()])
+transform_multi_augment = transforms.RandomOrder([transforms.RandomRotation(5), transforms.RandomHorizontalFlip()])
+
+transform_train_format = transforms.Compose([transforms.RandomChoice([transform_augment, transform_multi_augment], p=[0.25,0.15]), transform_format])
 
 # Dataset preparation
 # dataset = datasets.ImageFolder('Datasets', transform = transform_format)
 
-batch = 64
+batch = 32
 
-train_dataset = datasets.ImageFolder('Datasets\Train', transform_format)
-print(train_dataset.classes)
+train_dataset = datasets.ImageFolder('Datasets\Train', transform_train_format)
+print("Train dataset processed.")
+
 validate_dataset = datasets.ImageFolder('Datasets\Validate', transform_format)
-print(validate_dataset.classes)
+print("Validation dataset processed.")
+
 test_dataset = datasets.ImageFolder('Datasets\Test', transform_format)
-print(test_dataset.classes)
+print("Test dataset processed.")
 
 train_data_load = DataLoader(train_dataset, batch_size=batch, shuffle=True)
 validate_data_load = DataLoader(validate_dataset, batch_size=batch, shuffle=False)
@@ -62,14 +66,14 @@ test_data_load = DataLoader(test_dataset, batch_size=batch, shuffle=False)
 class cnn(nn.Module):
     def __init__(self):
         super(cnn, self).__init__()
-        self.conv_layer_1 = nn.Conv2d(1,8,3)
-        self.conv_layer_2 = nn.Conv2d(8,16,3)
+        self.conv_layer_1 = nn.Conv2d(1,16,3)
+        self.conv_layer_2 = nn.Conv2d(16,64,3)
         self.pool = nn.MaxPool2d(2,2)
-        self.fc_1 = nn.Linear(16*34*34, 128)
-        self.fc_2 = nn.Linear(128, 64)
-        self.fc_3 = nn.Linear(64,2)
-        self.dropout_1 = nn.Dropout(0.25)
-        self.dropout_2 = nn.Dropout(0.5)
+        self.fc_1 = nn.Linear(64*34*34, 256)
+        self.fc_2 = nn.Linear(256, 32)
+        self.fc_3 = nn.Linear(32,2)
+        self.dropout_1 = nn.Dropout(0.2)
+        self.dropout_2 = nn.Dropout(0.3)
     
     def forward(self, data):
         data = self.pool(nn.functional.relu(self.conv_layer_1(data)))
@@ -84,7 +88,7 @@ class cnn(nn.Module):
 
 # Early stopping
 class EarlyStopping:
-    def __init__(self, patience = 3, delta = 1):
+    def __init__(self, patience = 5, delta = 0.001):
         self.patience = patience
         self.delta = delta
         self.best_score = None
@@ -121,6 +125,14 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 print("Training start. Device: {}".format(device))
 
+early_stopping = EarlyStopping()
+
+# Train and validation loss and accuracy arrays
+train_loss_list = []
+valid_loss_list = []
+train_acc_list = []
+valid_acc_list = []
+
 # Training and validation loop
 for epoch in range(epoch_amt):
     #Training
@@ -148,8 +160,13 @@ for epoch in range(epoch_amt):
         train_loss += loss.item()
 
     train_accuracy = 100 * train_correct / len(train_dataset)
+    train_loss_avg = train_loss/len(train_data_load)
 
-    print("Training - Epoch {}, Accuracy: {},  Loss: {}".format(epoch, train_accuracy, train_loss))
+    print("Training - Epoch {}, Accuracy: {},  Loss: {}".format(epoch, train_accuracy, train_loss_avg))
+
+    train_acc_list.append(train_accuracy)
+    train_loss_list.append(train_loss_avg)
+
     train_loss = 0.0
 
     # Validation
@@ -169,11 +186,14 @@ for epoch in range(epoch_amt):
 
         valid_loss += loss.item()
 
+    valid_loss_avg = valid_loss/len(validate_data_load)
     valid_accuracy = 100 * valid_correct / len(validate_dataset)
-    print("Validation - Epoch {}, Accuracy: {},  Loss: {}".format(epoch, valid_accuracy, valid_loss))
+    print("Validation - Epoch {}, Accuracy: {},  Loss: {}".format(epoch, valid_accuracy, valid_loss_avg))
+
+    valid_acc_list.append(valid_accuracy)
+    valid_loss_list.append(valid_loss_avg)
 
     #Stop the training early
-    early_stopping = EarlyStopping()
     early_stopping(valid_loss, model)
     if early_stopping.early_stop:
         print("Training stopped.")
@@ -189,6 +209,7 @@ test_correct = 0
 
 with torch.no_grad():
     for i, (images, labels) in enumerate(test_data_load):
+        images, labels = images.to(device), labels.to(device)
         prediction = model(images)
         test_loss += criterion(prediction, labels).item()
 
@@ -198,3 +219,19 @@ test_loss /= batch
 test_accuracy = 100 * test_correct / len(test_dataset)
 
 print("Test - Accuracy: {},  Loss: {}".format(test_accuracy, test_loss))
+
+# Make train/validation graph
+train_acc_list = torch.tensor(train_acc_list, device='cuda').cpu().numpy()
+train_loss_list = torch.tensor(train_loss_list, device='cuda').cpu().numpy()
+valid_acc_list = torch.tensor(valid_acc_list, device='cuda').cpu().numpy()
+valid_loss_list = torch.tensor(valid_loss_list, device='cuda').cpu().numpy()
+
+plt.plot(train_acc_list, label = "Train accuracy")
+plt.plot(valid_acc_list, label = "Validation accuracy")
+plt.legend()
+plt.show()
+
+plt.plot(train_loss_list, label = "Train loss")
+plt.plot(valid_loss_list, label = "Validation loss")
+plt.legend()
+plt.show()
